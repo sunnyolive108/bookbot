b8a47beb486638f3e39e53003d3086012335f627
Anderson <Oliver.Anderson@loxxess.com>
2025-01-01 01:59:24 +0100

    first bookbot commit workstation

---
+++ main.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+def count_words(text):
+    
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    text = text.lower()
+
+    character_count = {}
+
+    for char in text:
+        if char.isalpha():
+            if char in character_count:
+                character_count[char] += 1
+
+            else:
+                character_count[char] = 1
+            
+    return character_count
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+
+    word_count = count_words(file_contents)
+    print(f"{word_count} words found in the document")
+
+    character_count = count_characters(file_contents)
+    for char, count in character_count.items():
+        print(f"The {char} character was found {count} times")
+
+
+if __name__ == "__main__":
+    main()
